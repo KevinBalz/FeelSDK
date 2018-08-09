942ecf6a73e433b04a5de2beffaef1dac8ec2e3c
Kevin Balz <kevin.balz@web.de>
2018-08-09 08:58:26 +0200

    Implement CCD for later use

---
+++ Assets/FeelSDK/Scripts/HandRig.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HandRig : MonoBehaviour
+{
+    public List<JointData> joints;
+    public Transform targetTransform;
+
+    void Update()
+    {
+        DoIK();
+    }
+
+    void DoIK()
+    {
+        var lastJoint = joints.Last();
+        Vector2 target = targetTransform.position;
+        Vector2 end = lastJoint.transform.position + lastJoint.transform.TransformDirection(lastJoint.endPointOffset);
+        Debug.Log(end);
+        foreach (var joint in joints.AsEnumerable().Reverse())
+        {
+            Vector2 position = joint.transform.position;
+            var toEnd = end - position;
+            var toTarget = target - position;
+            var endRot = Quaternion.FromToRotation(toEnd, toTarget);
+            var rot = joint.transform.rotation * endRot;
+            if (rot.eulerAngles.z >= joint.angleConstraint.x && rot.eulerAngles.z <= joint.angleConstraint.y)
+            {
+                joint.transform.rotation = rot;
+            }
+            end = lastJoint.transform.position + lastJoint.transform.TransformDirection(lastJoint.endPointOffset);
+        }
+    }
+
+    class BoneData
+    {
+        public Vector2 pos;
+        public float angle;
+        public float cosAngle;
+        public float sinAngle;
+    }
+}
---
+++ Assets/FeelSDK/Scripts/JointData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class JointData
+{
+    public Transform transform;
+    public Vector3 endPointOffset;
+    public Vector2 angleConstraint;
+}
